a657b36ffcd7ca66b767091655d89c16eefe1e6c
ajnanmvr <ajnanpulikkathody@gmail.com>
2022-08-29 19:26:22 +0530

    going to stash

--- script.js
+++ script.js
@@ -15,14 +15,14 @@ const filterButton = () => {
   for (let i = 0; i < btns.length; i++) {
     container.insertAdjacentHTML(
       "beforeend",
-      `<li class="${btns[i][0].replaceAll(' ','-')}" onclick="filterData('${btns[i][0]}')">${btns[i][1]}</li>`
+      `<li class="${btns[i][0].replaceAll(" ", "-")}" onclick="filterData('${
+        btns[i][0]
+      }')">${btns[i][1]}</li>`
     );
   }
 };
-filterButton();
 
-
-const studentData = [
+let studentData = [
   {
     name: "Muhammed Ajnan P",
     adno: 3127,
@@ -43,49 +43,18 @@ const studentData = [
     adno: 3129,
     section: "Degree Second Year",
   },
-  {
-    name: "Muhammed Mirshad",
-    adno: 3129,
-    section: "Degree Second Year",
-  },
- {
-    name: "Muhammed Mirshad",
-    adno: 3129,
-    section: "Degree Second Year",
-  },  {
-    name: "Muhammed Mirshad",
-    adno: 3129,
-    section: "Degree Second Year",
-  },  {
-    name: "Muhammed Mirshad",
-    adno: 3129,
-    section: "Degree Second Year",
-  },  {
-    name: "Muhammed Mirshad",
-    adno: 3129,
-    section: "Degree Second Year",
-  },  {
-    name: "Muhammed Mirshad",
-    adno: 3129,
-    section: "Degree Second Year",
-  },  {
-    name: "Muhammed Mirshad",
-    adno: 3129,
-    section: "Degree Second Year",
-  },
 ];
 
-filterData = (key) => {
-  let dataInfo = document.querySelector('.dataInfo')
+let filterData = (key) => {
+  let dataInfo = document.querySelector(".dataInfo");
   // Matching Data
   let finalData = studentData.filter((value) => value.section == key);
   // returns Complete Database is `$key` Doesn't Exsist
   if (!key) {
     finalData = studentData;
-    dataInfo.style.opacity = "0"
-  }else{
-    dataInfo.style.opacity = "1"
-
+    dataInfo.style.opacity = "0";
+  } else {
+    dataInfo.style.opacity = "1";
   }
 
   // Creating HTML
@@ -97,14 +66,14 @@ filterData = (key) => {
             <div class="data_header">
                 <p class="heading">Member Identity Card</p>
                 <p class="isActive">Active Member</p>
-            </div>
+                </div>
             <div class="id_data">
                 <div class="id_image" id="image"
                     style="background:rgba(255, 255, 0, 0.247) url('./data/img/${adno}.jpg')" alt="Student Image"></div>
                 <p class="name_adno_container">
                     <span class="name_container">Name: <span class="name">${name}</span> </span>
                     <span class="adno_container">Ad.No: <span class="adno">${adno}</span> </span>
-                </p>
+                    </p>
                 <p class="section_container">Section: <span class="section">${section}</span> </p>
                 <div class="data_footer">
                     <p class="ValidDate_container">Valid Upto: <span class="section">07-06-2023</span> </p>
@@ -115,33 +84,32 @@ filterData = (key) => {
         <div class="id_back"></div>
     </div>
     `;
-    
   });
 
   // Printing HTML
   document.querySelector("main").innerHTML = layout;
 
-    
-
   // Printing Info
-    let myInfo = document.querySelector('.info')
-  let newInfo =`
+  let myInfo = document.querySelector(".info");
+  let newInfo = `
   <table>
     <tr class="filteredData">
-      <th>Filtered Data:</th>
-      <td>${finalData.length}</td>
+    <th>Filtered Data:</th>
+    <td>${finalData.length}</td>
     </tr>
     <tr>
     <th>Filtering By:</th>
     <td>${key}</td>
-  </tr>
+    </tr>
     <tr class="totalData">
       <th>Total Data:</th>
       <td>${studentData.length}</td>
     </tr>
-  </table>`
+  </table>`;
 
-  myInfo.innerHTML = newInfo
+  myInfo.innerHTML = newInfo;
 };
 
 filterData();
+
+filterButton();
